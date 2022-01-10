249cbdf75e845e45690a3bc9ae34cac3f3b63105
Alvaro <alvaroriba585@gmail.com>
2022-01-10 11:40:51 -0300

    6 commit

---
+++ data_type_exp/list.py
@@ -0,0 +1,17 @@
+colors = ['red','green','blue','orange','violet','brown'];
+
+# List Constructor
+# nmbList = list((1,2,3,4,5))
+# print(nmbList)
+
+# #Range
+# rList = list(range(1,11));
+# print(rList)
+
+#Methods
+#print(dir(colors))
+
+print('red' in colors)
+
+colors[2] = 'aqua'
+print(colors)
