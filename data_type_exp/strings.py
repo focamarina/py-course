840d0d91767a7e2053effe82f05cacfe2c5cbf21
Alvaro <alvaroriba585@gmail.com>
2022-01-10 10:45:42 -0300

    4 commit

--- data_type_exp/strings.py
+++ data_type_exp/strings.py
@@ -1,16 +1,33 @@
-myStr = 'hello world';
+myStr = 'foca';
+print(f"My name is {myStr}")
+print("My name is {0}".format(myStr))
 
 ##############################################################
 
 #View the methods of str
-print(dir(myStr));
+# print(dir(myStr));
 
 ##############################################################
 
 #Apply the str methods
-print(myStr.upper());
-print(myStr.title());
-print(myStr.lower());
-print(myStr.swapcase());
-print(myStr.capitalize());
-print(myStr.replace('hello','bye').upper());
+# print(myStr.upper());
+# print(myStr.title());
+# print(myStr.lower());
+# print(myStr.swapcase());
+# print(myStr.capitalize());
+# print(myStr.replace('hello','bye').upper());
+# print(myStr.count('l'));
+
+# print(myStr.startswith('hello'));
+# print(myStr.endswith('world'));
+
+# print(myStr.split());
+# print(myStr.find('o'));
+
+# print(len(myStr));
+# print(myStr.index('e'));
+
+# print(myStr.isnumeric());
+# print(myStr.isalpha());
+
+# print(myStr[4]);
