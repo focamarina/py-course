d85d2970a38e02f3bf13316a1d12c6d646922461
Alvaro <alvaroriba585@gmail.com>
2022-01-11 12:24:21 -0300

    Final Commit - 13

---
+++ Modules/modules.py
@@ -0,0 +1,13 @@
+# https://docs.python.org/3/py-modindex.html
+# https://pypi.org/
+
+import datetime
+import fmath
+from colorama import Fore, Style
+
+print(Fore.CYAN + "Hello Bitch!")
+
+print(datetime.date.today())
+print(datetime.timedelta(minutes=90))
+
+print(fmath.add(10,54))
