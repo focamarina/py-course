383964c5f6664b8d8a30e3436f01b6c6b410889a
Alvaro <alvaroriba585@gmail.com>
2022-01-11 11:46:29 -0300

    12 commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+# def hello(name):
+#     print(f"Welcome {name}! Please enjoy our programme")
+
+# hello(input("Please, enter your name: "))
+
+# def add(n1,n2):
+#     return n1+n2
+
+# print(add(10,54))
+
+add = lambda n1,n2: n1+n2
+
+print(add(45,62))
