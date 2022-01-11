15ff49cfc4fe23e1f65c257432fcecc64c33a5b9
Alvaro <alvaroriba585@gmail.com>
2022-01-11 10:36:51 -0300

    9 commit

---
+++ data_type_exp/dicts.py
@@ -0,0 +1,17 @@
+product = {
+    "name":"book",
+    "price":4.99,
+    "quantity":3
+}
+
+#print(dir(product))
+
+print(product.keys())
+print(product.items())
+
+cart = [
+    {"name":"pen", "quantity":1, "price":2.45},
+    {"name":"notebook", "quantity":2, "price":800}
+]
+
+print(cart)
