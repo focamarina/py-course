ad0e07be02a45235bb7a276a17fda57725b21447
Alvaro <alvaroriba585@gmail.com>
2022-01-11 11:29:10 -0300

    11 commit

---
+++ bucles.py
@@ -0,0 +1,12 @@
+# foods = ["Apple","Orange","Pizza","Hamburguer","Bread","Milk"]
+
+# for food in foods:
+#     if food == "Bread":
+#         continue
+#     print(food);
+
+count = 1
+
+while count <= 10:
+    print(count)
+    count = count+1
