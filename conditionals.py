9a718451efb90c9fbcf6ff86fe201be64171feea
Alvaro <alvaroriba585@gmail.com>
2022-01-11 11:13:38 -0300

    10 commit

---
+++ conditionals.py
@@ -0,0 +1,22 @@
+# x = input("Inserte un numero del 1 al 100: ");
+# x = int(x)
+
+# y = input("Inserte otro numero del 1 al 100: ")
+# y = int(y)
+
+# if x < y:
+#     print(f"{x} es menor a {y}")
+# elif x == y:
+#     print(f"{x} es igual a {y}")
+# elif x > y:
+#     print(f"{x} es mayor que {y}")
+# else:
+#     print("Error!")
+
+x = 5
+if x > 2 and x <= 10:
+    print("ICKKCK")
+if x > 2 or x <= 20:
+    print("ICKKC")
+if (not(x == 25)):
+    print("ICKK")
